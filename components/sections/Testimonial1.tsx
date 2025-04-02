c8d43301c3b0b50e42bf03649a19c90ebfecc720
GURO-doctorm <miloudelgoumadi@gmail.com>
2025-04-02 12:20:19 +0000

    add some reviews to last section

--- components/sections/Testimonial1.tsx
+++ components/sections/Testimonial1.tsx
@@ -14,8 +14,8 @@ export default function Testimonial1() {
 									<img src="/assets/imgs/features-1/dots.png" alt="infinia" />
 									<span className="tag-spacing fs-7 fw-bold text-linear-2 ms-2 text-uppercase">Testimonials</span>
 								</div>
-								<h3 className="ds-3 mt-3 mb-3" data-aos="fade-zoom-in" data-aos-delay={100}>What our clients say</h3>
-								<span className="fs-5 fw-medium" data-aos="fade-zoom-in" data-aos-delay={300}>Access top-tier group mentoring plans and exclusive professional benefits for your team. 🔥</span>
+								<h3 className="ds-3 mt-3 mb-3" data-aos="fade-zoom-in" data-aos-delay={100}>User Reviews</h3>
+								<span className="fs-5 fw-medium" data-aos="fade-zoom-in" data-aos-delay={300}>Curious how our solution can make a difference? Check out these testimonials from satisfied customers.🔥</span>
 								<div className="d-flex flex-wrap align-items-center mt-8">
 									<Link href="/page-contact-1" className="btn btn-outline-secondary hover-up" data-aos="fade-zoom-in" data-aos-delay={100}>
 										<svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none">
@@ -33,28 +33,28 @@ export default function Testimonial1() {
 						</div>
 						<div className="col-lg-4">
 							<div className="bg-neutral-100 p-5 mt-8 rounded-3 position-relative card-hover" data-aos="fade-zoom-in" data-aos-delay={100}>
-								<p className="text-900">The team's dedication and expertise have transformed our business. <span className="fw-bold">Their innovative solutions and outstanding support</span> have significantly boosted our productivity and client satisfaction. Allowing us to streamline our processes and focus on what matters most.</p>
+								<p className="text-900">I no longer waste time designing pages from scratch. <span className="fw-bold">Now, I can launch multiple products fast and see which one performs best.</span> A must-have for every eCommerce seller.</p>
 								<div className="d-flex align-items-center mt-5">
 									<img className="avatar-lg" src="/assets/imgs/testimonial-1/avatar-1.png" alt="infinia" />
 									<div className="d-flex flex-column">
-										<h6 className="ms-3 fs-6 mb-0">Kathryn Murphy</h6>
+										<h6 className="ms-3 fs-6 mb-0">Rayan Al-Farsi</h6>
 										<div className="flag ms-3">
-											<img src="/assets/imgs/testimonial-1/flag-1.png" alt="infinia" />
-											<span className="fs-8">CEO of Tech Innovators Inc</span>
+											{/*<img src="/assets/imgs/testimonial-1/flag-1.png" alt="infinia" />*/}
+											<span className="fs-8">Dropshipper</span>
 										</div>
 									</div>
 								</div>
 								<Link href="#" className="position-absolute top-0 start-0 end-0 bottom-0" />
 							</div>
 							<div className="bg-neutral-100 p-5 mt-5 rounded-3 position-relative card-hover" data-aos="fade-zoom-in" data-aos-delay={200}>
-								<p className="text-900">Our collaboration with the team has been instrumental in optimizing our project management processes. <span className="fw-bold">The extensive selection of over 1200 UI blocks has allowed us to customize our project interfaces to meet specific client needs effectively.</span> The generous 10 GB of cloud storage has provided ample space for storing project files securely, enabling seamless collaboration across distributed teams.</p>
+								<p className="text-900">I know the importance of a well-optimized landing page. But building them from scratch was time-consuming and slowed down my campaigns. <span className="fw-bold">With this tool, </span> I can create stunning, conversion-focused pages in minutes. The designs are professional, fully customizable, and built for results. Now, I can focus on driving traffic instead of struggling with page creation.</p>
 								<div className="d-flex align-items-center mt-5">
 									<img className="avatar-lg" src="/assets/imgs/testimonial-1/avatar-2.png" alt="infinia" />
 									<div className="d-flex flex-column">
-										<h6 className="ms-3 fs-6 mb-0">Emily Davis</h6>
+										<h6 className="ms-3 fs-6 mb-0">Ethan Parker</h6>
 										<div className="flag ms-3">
-											<img src="/assets/imgs/testimonial-1/flag-2.png" alt="infinia" />
-											<span className="fs-8">Software Engineer</span>
+											{/*<img src="/assets/imgs/testimonial-1/flag-2.png" alt="infinia" />*/}
+											<span className="fs-8">Brand owner</span>
 										</div>
 									</div>
 								</div>
@@ -63,28 +63,28 @@ export default function Testimonial1() {
 						</div>
 						<div className="col-lg-4">
 							<div className="bg-neutral-100 p-5 mt-5 mt-lg-0 rounded-3 position-relative card-hover" data-aos="fade-zoom-in" data-aos-delay={300}>
-								<p className="text-900">The individual email account feature has improved internal communication clarity and professionalism. Moreover, the premium support team's responsiveness and expertise have ensured minimal <span className="fw-bold">disruptions and quick resolutions</span> to any technical challenges we've faced. I highly recommend their services for any enterprise seeking robust SaaS solutions,</p>
+								<p className="text-900">Testing multiple products used to be a slow and frustrating process. Creating landing pages from scratch took hours, delaying my ability to find winning products.<span className="fw-bold">But with this tool, I can generate high-quality, professional pages in minutes—ready to convert and scale. </span> The designs feel like big-brand websites, and I can quickly test variations to find what works. It has streamlined my workflow, letting me focus on scaling instead of building pages.</p>
 								<div className="d-flex align-items-center mt-5">
 									<img className="avatar-lg" src="/assets/imgs/testimonial-1/avatar-3.png" alt="infinia" />
 									<div className="d-flex flex-column">
-										<h6 className="ms-3 fs-6 mb-0">Sarah Thompson</h6>
+										<h6 className="ms-3 fs-6 mb-0">Nour Al-Shehri</h6>
 										<div className="flag ms-3">
-											<img src="/assets/imgs/testimonial-1/flag-3.png" alt="infinia" />
-											<span className="fs-8">Marketing Director</span>
+											{/*<img src="/assets/imgs/testimonial-1/flag-3.png" alt="infinia" />*/}
+											<span className="fs-8">COD Seller in GCC</span>
 										</div>
 									</div>
 								</div>
 								<Link href="#" className="position-absolute top-0 start-0 end-0 bottom-0" />
 							</div>
 							<div className="bg-neutral-100 p-5 mt-5 rounded-3 position-relative card-hover" data-aos="fade-zoom-in" data-aos-delay={400}>
-								<p className="text-900">Our experience with this team has surpassed our expectations on every front. <span className="fw-bold">The comprehensive suite of over 1200 UI blocks has enabled us to craft highly functional</span> and aesthetically pleasing user interfaces that resonate with our target audience. Equally impressive is the premium support team's proactive approach.</p>
+								<p className="text-900">As a busy entrepreneur, I needed a tool that helped me save time and stay productive. With this tool, <span className="fw-bold">I can generate high-quality landing pages quickly,</span> allowing me to focus on my business growth instead of spending endless hours on page creation.</p>
 								<div className="d-flex align-items-center mt-5">
 									<img className="avatar-lg" src="/assets/imgs/testimonial-1/avatar-4.png" alt="infinia" />
 									<div className="d-flex flex-column">
-										<h6 className="ms-3 fs-6 mb-0">Emily Adams</h6>
+										<h6 className="ms-3 fs-6 mb-0">James Wong</h6>
 										<div className="flag ms-3">
-											<img src="/assets/imgs/testimonial-1/flag-4.png" alt="infinia" />
-											<span className="fs-8">Software Engineer</span>
+											{/*<img src="/assets/imgs/testimonial-1/flag-4.png" alt="infinia" />*/}
+											<span className="fs-8">Digital entrepreneur</span>
 										</div>
 									</div>
 								</div>
