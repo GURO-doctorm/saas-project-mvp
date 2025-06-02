a6524e194b55581def527e249c1e8afc5de34b9c
GURO-doctorm <miloudelgoumadi@gmail.com>
2025-06-02 18:16:07 +0100

    editing the navbar and footer is done

--- components/layout/footer/Footer1.tsx
+++ components/layout/footer/Footer1.tsx
@@ -33,50 +33,30 @@ export default function Footer1() {
 										<div className="col-lg-3 col-md-4 col-6" data-aos="fade-zoom-in" data-aos-delay={200}>
 											<h3 className="text-white opacity-50 fs-6 fw-black text-uppercase pb-3 pt-5">Company</h3>
 											<div className="d-flex flex-column align-items-start">
-												<Link className="hover-effect text-white mb-2 fw-medium fs-6" href="#">Mission  Vision</Link>
-												<Link className="hover-effect text-white mb-2 fw-medium fs-6" href="#">Our Team</Link>
-												<Link className="hover-effect text-white mb-2 fw-medium fs-6" href="#">Careers</Link>
-												<Link className="hover-effect text-white mb-2 fw-medium fs-6" href="#">Press  Media</Link>
-												<Link className="hover-effect text-white mb-2 fw-medium fs-6" href="#">Advertising</Link>
-												<Link className="hover-effect text-white mb-2 fw-medium fs-6" href="#">Testimonials</Link>
+												<Link className="hover-effect text-white mb-2 fw-medium fs-6" href="/page-about-2">About Us</Link>
+												<Link className="hover-effect text-white mb-2 fw-medium fs-6" href="/page-team-4">Our Team</Link>
+												<Link className="hover-effect text-white mb-2 fw-medium fs-6" href="/page-contact-3">Contact Us</Link>
 											</div>
 										</div>
 										<div className="col-lg-3 col-md-4 col-6" data-aos="fade-zoom-in" data-aos-delay={300}>
-											<h3 className="text-white opacity-50 fs-6 fw-black text-uppercase pb-3 pt-5">Guides</h3>
+											<h3 className="text-white opacity-50 fs-6 fw-black text-uppercase pb-3 pt-5">Explore</h3>
 											<div className="d-flex flex-column align-items-start">
-												<Link className="hover-effect text-white mb-2 fw-medium fs-6" href="#">Our Journeys</Link>
-												<Link className="hover-effect text-white mb-2 fw-medium fs-6" href="#">Solutions</Link>
-												<Link className="hover-effect text-white mb-2 fw-medium fs-6" href="#">Customers</Link>
-												<Link className="hover-effect text-white mb-2 fw-medium fs-6" href="#">News  Events</Link>
-												<Link className="hover-effect text-white mb-2 fw-medium fs-6" href="#">Project management</Link>
-												<Link className="hover-effect text-white mb-2 fw-medium fs-6" href="#">Help Center</Link>
+												<Link className="hover-effect text-white mb-2 fw-medium fs-6" href="/page-pricing-01">Pricing</Link>
+												<Link className="hover-effect text-white mb-2 fw-medium fs-6" href="/page-integration">Integrations</Link>
 											</div>
 										</div>
 										<div className="col-lg-3 col-md-4 col-6" data-aos="fade-zoom-in" data-aos-delay={400}>
-											<h3 className="text-white opacity-50 fs-6 fw-black text-uppercase pb-3 pt-5">Services</h3>
+											<h3 className="text-white opacity-50 fs-6 fw-black text-uppercase pb-3 pt-5">Legal Info</h3>
 											<div className="d-flex flex-column align-items-start">
-												<Link className="hover-effect text-white mb-2 fw-medium fs-6" href="#">Project software</Link>
-												<Link className="hover-effect text-white mb-2 fw-medium fs-6" href="#">Resource software</Link>
-												<Link className="hover-effect text-white mb-2 fw-medium fs-6" href="#">Workflow</Link>
-												<Link className="hover-effect text-white mb-2 fw-medium fs-6" href="#">Gantt chart makers</Link>
-												<Link className="hover-effect text-white mb-2 fw-medium fs-6" href="#">Project dashboards</Link>
-												<Link className="hover-effect text-white mb-2 fw-medium fs-6" href="#">Task software</Link>
-											</div>
-										</div>
-										<div className="col-lg-3 col-md-4 col-6" data-aos="fade-zoom-in" data-aos-delay={500}>
-											<h3 className="text-white opacity-50 fs-6 fw-black text-uppercase pb-3 pt-5">Resource</h3>
-											<div className="d-flex flex-column align-items-start">
-												<Link className="hover-effect text-white mb-2 fw-medium fs-6" href="#">Knowledge Base</Link>
-												<Link className="hover-effect text-white mb-2 fw-medium fs-6" href="#">Documents</Link>
-												<Link className="hover-effect text-white mb-2 fw-medium fs-6" href="#">System Status</Link>
-												<Link className="hover-effect text-white mb-2 fw-medium fs-6" href="#">Security</Link>
+												<Link className="hover-effect text-white mb-2 fw-medium fs-6" href="/page-privacy-policy">Privacy Policy</Link>
+												<Link className="hover-effect text-white mb-2 fw-medium fs-6" href="/page-terms-conditions">Terms & Conditions</Link>
 											</div>
 										</div>
 									</div>
 								</div>
 							</div>
 							<div className="row text-center py-4 border-top border-white border-opacity-10">
-								<span className="text-white opacity-50" data-aos="fade-zoom-in" data-aos-delay={200}>Copyright © 2024 Infinia. All Rights Reserved</span>
+								<span className="text-white opacity-50" data-aos="fade-zoom-in" data-aos-delay={200}>Copyright © 2025 Infinia. All Rights Reserved</span>
 							</div>
 						</div>
 					</div>
